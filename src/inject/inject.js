2ee9e9b3421a7246df52020ade89e52b5ed464fd
Ivan <isedletzki@scaleway.com>
2024-07-02 00:00:54 +0200

    wow i was such a bad coder 22 months ago

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,43 +1,40 @@
+const blackList = [
+	// It just isn't fun anymore
+	'9gag.com',
+]
+
+const greyList = [
+	// youtube = productivity killer
+	'www.youtube.com',
+
+	// comics I like a bit too much
+	'www.commitstrip.com',
+	'xkcd.com',
+	'www.smbc-comics.com',
+]
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 
 		// date limitations
 		const d = new Date()
-		if(
-			d.getDay() == 6 || d.getDay() == 0 // satuday, sunday
-			|| d.getHours() >= 18 // after 6pm
-		) {
-			return
-		}
+
+		// also block after midnight
+		const isWorkHours = d.getDay() != 6 && d.getDay() != 0 && d.getHours() < 18
 
 		if (document.readyState === "complete") {
 			clearInterval(readyStateCheckInterval);
 
-			switch(window.location.hostname) {
-				// yt
-				case 'www.youtube.com':
-
-				// memes
-				case '9gag.com':
-
-				// games
-				case "www.jeuxvideo.com":
-
-				// comics (that i like a bit too much)
-				case "www.commitstrip.com":
-				case "dilbert.com":
-				case "xkcd.com":
-				case "www.smbc-comics.com":
-					const a = Math.floor(1 + Math.random() * 10)
-					const b = Math.floor(1 + Math.random() * 10)
-					const res = -1 // window.prompt(`${a} * ${b}?`)
-					
-					if(res != a * b) {
-						window.location.href = "https://www.meme-arsenal.com/memes/308414a4ed2d3aceb7b323dccba5193e.jpg"
-					}
-
-					return
+			if(blackList.includes(window.location.hostname)) {
+				window.location.href = "https://www.meme-arsenal.com/memes/308414a4ed2d3aceb7b323dccba5193e.jpg"
+				return
 			}
+
+			if(greyList.includes(window.location.hostname) && !isWorkHours) {
+				window.location.href = "https://www.meme-arsenal.com/memes/308414a4ed2d3aceb7b323dccba5193e.jpg"
+			}
+
+			return
 		}
 	}, 10);
 });
